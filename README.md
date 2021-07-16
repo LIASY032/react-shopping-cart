719c9061e6698e11dde75171aa5a7a2c274fcc93
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-07-17 02:02:27 +0930

    deploy on heroku

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #React Shopping Cart
+
+git
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const shortid = require("shortid");
 
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+app.use("/", express.static(__dirname + "/build"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost/react-shopping-cart-db",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const Product = mongoose.model(
   "products",
