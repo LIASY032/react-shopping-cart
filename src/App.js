2e6a0b43c782269b59ca1b01b7efad45e20914b1
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-07-16 23:41:13 +0930

    create order

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // feature 1
 import React from "react";
-import data from "./data.json";
+
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 import Cart from "./components/Cart";
